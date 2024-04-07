f06785f7e1dd3c18b80581d6f8f2a08387e0a8ec
MAINAK CHAUDHURI <sultankhilji001@gmail.com>
2024-04-07 20:34:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,8 +26,10 @@ def prediction_cls(prediction):
             return key
 
 @st.cache(allow_output_mutation=True)
+      
 def load_model():
     model = tf.keras.models.load_model('mango_model.h5')
+    custom_objects={'KerasLayer':hub.KerasLayer}
     return model
 
 with st.spinner('Model is being loaded..'):
