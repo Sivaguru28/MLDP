29723940ad0def0c22da66d065f45f907b0a6e43
MAINAK CHAUDHURI <sultankhilji001@gmail.com>
2024-04-12 18:42:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,8 +26,6 @@ def prediction_cls(prediction):
             return key
 
 @st.cache(allow_output_mutation=True)
-      
-@st.cache(allow_output_mutation=True)
 def load_model():
     model=tf.keras.models.load_model('mango_model.h5')
     return model
